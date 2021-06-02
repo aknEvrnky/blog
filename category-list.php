c73d912a5b90c5bbdb748401be9b2bc61d4e510c
aknEvrnky <evrenkaya.2002.save@gmail.com>
2021-06-02 14:27:43 +0300

    kategori listeleme bitti

--- category-list.php
+++ category-list.php
@@ -1,10 +1,216 @@
 <?php include 'includes/header.php'; ?>
 
-<?php $title = 'Blog Detayı' ?>
+<?php $title = 'Kategori Listeleme' ?>
 
 <?php include 'includes/breadcrumb.php'; ?>
 
 
+<section class="category-list">
+    <div class="container">
+        <div class="row row-area">
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-card">
+                    <a href="#" title="blog" class="blog-img">
+                        <img src="front/images/blog3.jpg" alt="" class="img-fluid">
+                        <div class="btn-blue">Bloğa Git</div>
+                    </a>
+                    <div class="stats">
+                        <a href="#" title="5 yorum"><i class="fas fa-comment"></i>5</a>
+                        <span title="341 görüntülenme"><i class="fal fa-eye"></i>341</span>
+                        <span title="2 dakika"><i class="fas fa-clock"></i>2</span>
+                    </div>
+                    <div class="blog-content">
+                        <div class="author-area">
+                            <div class="author">
+                                <img src="front/images/user.jpeg" alt="">
+                                <a href="#" title="Akın">Akın Evrenkaya</a>
+                            </div>
+                            <div class="date">
+                                31.05.2021
+                            </div>
+                        </div>
+                        <a href="#" class="title">Frontend döküm esnasında karşılaşılabilecek sorunlar</a>
+                        <div class="content-wrapper">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ipsam iure maxime quos, similique vitae...</p>
+                        </div>
+                        <div class="category-area">
+                            <i class="fal fa-tags"></i>
+                            <a href="#" title="">Oyun,</a>
+                            <a href="#" title="">PHP</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-card">
+                    <a href="#" title="blog" class="blog-img">
+                        <img src="front/images/blog3.jpg" alt="" class="img-fluid">
+                        <div class="btn-blue">Bloğa Git</div>
+                    </a>
+                    <div class="stats">
+                        <a href="#" title="5 yorum"><i class="fas fa-comment"></i>5</a>
+                        <span title="341 görüntülenme"><i class="fal fa-eye"></i>341</span>
+                        <span title="2 dakika"><i class="fas fa-clock"></i>2</span>
+                    </div>
+                    <div class="blog-content">
+                        <div class="author-area">
+                            <div class="author">
+                                <img src="front/images/user.jpeg" alt="">
+                                <a href="#" title="Akın">Akın Evrenkaya</a>
+                            </div>
+                            <div class="date">
+                                31.05.2021
+                            </div>
+                        </div>
+                        <a href="#" class="title">Frontend döküm esnasında karşılaşılabilecek sorunlar</a>
+                        <div class="content-wrapper">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ipsam iure maxime quos, similique vitae...</p>
+                        </div>
+                        <div class="category-area">
+                            <i class="fal fa-tags"></i>
+                            <a href="#" title="">Oyun,</a>
+                            <a href="#" title="">PHP</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-card">
+                    <a href="#" title="blog" class="blog-img">
+                        <img src="front/images/blog3.jpg" alt="" class="img-fluid">
+                        <div class="btn-blue">Bloğa Git</div>
+                    </a>
+                    <div class="stats">
+                        <a href="#" title="5 yorum"><i class="fas fa-comment"></i>5</a>
+                        <span title="341 görüntülenme"><i class="fal fa-eye"></i>341</span>
+                        <span title="2 dakika"><i class="fas fa-clock"></i>2</span>
+                    </div>
+                    <div class="blog-content">
+                        <div class="author-area">
+                            <div class="author">
+                                <img src="front/images/user.jpeg" alt="">
+                                <a href="#" title="Akın">Akın Evrenkaya</a>
+                            </div>
+                            <div class="date">
+                                31.05.2021
+                            </div>
+                        </div>
+                        <a href="#" class="title">Frontend döküm esnasında karşılaşılabilecek sorunlar</a>
+                        <div class="content-wrapper">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ipsam iure maxime quos, similique vitae...</p>
+                        </div>
+                        <div class="category-area">
+                            <i class="fal fa-tags"></i>
+                            <a href="#" title="">Oyun,</a>
+                            <a href="#" title="">PHP</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-card">
+                    <a href="#" title="blog" class="blog-img">
+                        <img src="front/images/blog3.jpg" alt="" class="img-fluid">
+                        <div class="btn-blue">Bloğa Git</div>
+                    </a>
+                    <div class="stats">
+                        <a href="#" title="5 yorum"><i class="fas fa-comment"></i>5</a>
+                        <span title="341 görüntülenme"><i class="fal fa-eye"></i>341</span>
+                        <span title="2 dakika"><i class="fas fa-clock"></i>2</span>
+                    </div>
+                    <div class="blog-content">
+                        <div class="author-area">
+                            <div class="author">
+                                <img src="front/images/user.jpeg" alt="">
+                                <a href="#" title="Akın">Akın Evrenkaya</a>
+                            </div>
+                            <div class="date">
+                                31.05.2021
+                            </div>
+                        </div>
+                        <a href="#" class="title">Frontend döküm esnasında karşılaşılabilecek sorunlar</a>
+                        <div class="content-wrapper">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ipsam iure maxime quos, similique vitae...</p>
+                        </div>
+                        <div class="category-area">
+                            <i class="fal fa-tags"></i>
+                            <a href="#" title="">Oyun,</a>
+                            <a href="#" title="">PHP</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-card">
+                    <a href="#" title="blog" class="blog-img">
+                        <img src="front/images/blog3.jpg" alt="" class="img-fluid">
+                        <div class="btn-blue">Bloğa Git</div>
+                    </a>
+                    <div class="stats">
+                        <a href="#" title="5 yorum"><i class="fas fa-comment"></i>5</a>
+                        <span title="341 görüntülenme"><i class="fal fa-eye"></i>341</span>
+                        <span title="2 dakika"><i class="fas fa-clock"></i>2</span>
+                    </div>
+                    <div class="blog-content">
+                        <div class="author-area">
+                            <div class="author">
+                                <img src="front/images/user.jpeg" alt="">
+                                <a href="#" title="Akın">Akın Evrenkaya</a>
+                            </div>
+                            <div class="date">
+                                31.05.2021
+                            </div>
+                        </div>
+                        <a href="#" class="title">Frontend döküm esnasında karşılaşılabilecek sorunlar</a>
+                        <div class="content-wrapper">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ipsam iure maxime quos, similique vitae...</p>
+                        </div>
+                        <div class="category-area">
+                            <i class="fal fa-tags"></i>
+                            <a href="#" title="">Oyun,</a>
+                            <a href="#" title="">PHP</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-card">
+                    <a href="#" title="blog" class="blog-img">
+                        <img src="front/images/blog3.jpg" alt="" class="img-fluid">
+                        <div class="btn-blue">Bloğa Git</div>
+                    </a>
+                    <div class="stats">
+                        <a href="#" title="5 yorum"><i class="fas fa-comment"></i>5</a>
+                        <span title="341 görüntülenme"><i class="fal fa-eye"></i>341</span>
+                        <span title="2 dakika"><i class="fas fa-clock"></i>2</span>
+                    </div>
+                    <div class="blog-content">
+                        <div class="author-area">
+                            <div class="author">
+                                <img src="front/images/user.jpeg" alt="">
+                                <a href="#" title="Akın">Akın Evrenkaya</a>
+                            </div>
+                            <div class="date">
+                                31.05.2021
+                            </div>
+                        </div>
+                        <a href="#" class="title">Frontend döküm esnasında karşılaşılabilecek sorunlar</a>
+                        <div class="content-wrapper">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ipsam iure maxime quos, similique vitae...</p>
+                        </div>
+                        <div class="category-area">
+                            <i class="fal fa-tags"></i>
+                            <a href="#" title="">Oyun,</a>
+                            <a href="#" title="">PHP</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
+
+<?php include 'includes/textarea.php'; ?>
 
 <?php include 'includes/footer.php'; ?>
---
+++ includes/textarea.php
@@ -0,0 +1,23 @@
+<section class="default">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="content-wrapper">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda, beatae consectetur dolore earum est excepturi impedit mollitia, necessitatibus, nemo nihil nisi officiis quas quia quibusdam repellat rerum tempora voluptatem?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias aspernatur culpa deleniti earum enim fugiat impedit incidunt minima praesentium quasi, quisquam ut velit? A commodi ducimus hic officiis quasi. Accusantium dolore officiis ratione sint.</p>
+                    <ul>
+                        <li>
+                            Lorem ipsum dolor sit amet.
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </li>
+                        <li>
+                            Lorem ipsum dolor.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
